1f8e49b190d060022587169150172f551cb2e783
dexterbt1 <dtady@cpan.org>
2010-09-05 01:54:41 +0800

    added DISPLAY_FK support for display specific foreign key joins

--- src/breadpy/settings.py
+++ src/breadpy/settings.py
@@ -3,3 +3,12 @@ BASE_URL = ""
 DATABASE_ENGINE_URL = 'mysql://root:@localhost/spree?charset=utf8'
 TEMPLATE_APP_NAME = "breadpy"
 TEMPLATE_DIR_NAME = "templates"
+
+DISPLAY_FK = {
+    u'store_store' : {
+        u'user_id' : [ u'auth_user.username', u'auth_user.email' ],
+        },
+    u'store_order' : {
+        u'buyer_user_id' : [ u'auth_user.username', u'auth_user.email' ],
+        },
+}
--- src/breadpy/views/rows.py
+++ src/breadpy/views/rows.py
@@ -1,17 +1,51 @@
 # -*- coding: utf-8 -*-
+import breadpy.settings as settings
 import breadpy.db as db
 from breadpy.templates import get_template
 from sqlalchemy import MetaData, Table
+from sqlalchemy.sql import select
 
 def browse(table_name):
-    """ Show list of rows of the given table_name"""
-    # query
+    """ Show list of rows of the given table_name """
     meta = MetaData()
-    table = Table(table_name, meta, autoload=True, autoload_with=db.engine)
-    result = db.engine.execute(table.select())
-    rows = result.fetchall()
+    meta.reflect(db.engine)
+    #table = Table(table_name, meta, autoload=True, autoload_with=db.engine)
+    table = meta.tables[table_name]
+
+    # setup query
+    q_from = table
+    columns = [ ]
+
+    for tc in table.c.keys():
+        q_c = table.c.get(tc)
+        columns.append(q_c)
+        if hasattr(settings, 'DISPLAY_FK'):
+            # check if we need to join other tables
+            try:
+                join = set()
+                fks = settings.DISPLAY_FK[table_name][tc]
+                for fk in fks:
+                    fktablename, fkcolname = fk.split(".", 2)
+                    fktable = meta.tables[fktablename]
+                    join.add(fktable)
+                    fkcol = getattr(fktable.c, fkcolname)
+                    fkcol.breadpy_fk = True
+                    columns.append(fkcol)
+                for jt in join:
+                    q_from = q_from.join(jt)
+            except KeyError, ke:
+                pass
+                
+    q = select(from_obj=q_from, columns=columns)
+    sql = "%s" % q
+
+    # execute and fetch
+    result = db.engine.execute(q)
+    raw_rows = result.fetchall()
+    rows = raw_rows
 
     template = get_template("rows/browse.html")
-    return template.render(table=table, rows=rows)
+
+    return template.render(table=table, rows=rows, columns=columns, sql=sql)
 
 
